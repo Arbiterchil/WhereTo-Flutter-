123e886a69c0f1989af7f7e231506fce001e002c
Arbiterchil <arbiterchil@gmail.com>
2020-06-16 01:59:26 +0800

    Search Filter Done incomming OnTap Restaurant

--- lib/restaurants/restaurant_searchdepo.dart
+++ lib/restaurants/restaurant_searchdepo.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:WhereTo/api/api.dart';
+import 'package:WhereTo/restaurants/restaurant.dart';
 import 'package:flutter/material.dart';
 
 class SearchDepo extends StatefulWidget {
@@ -5,11 +9,49 @@ class SearchDepo extends StatefulWidget {
   _SearchDepoState createState() => _SearchDepoState();
 }
 
+    
+
+   Future<List<Restaurant>> _getRest() async{
+
+    var response = await ApiCall().getRestarant('/getFeaturedRestaurant');
+    
+      List<Restaurant> rests = [];
+    
+
+      var bods = json.decode(response.body);
+      
+      for(var bods in bods){
+        
+        Restaurant rest = Restaurant
+        (bods["id"],
+        bods["restaurantName"],
+        bods["address"],
+        bods["contactNumber"],
+        bods["isFeatured"]);
+        rests.add(rest);
+      }
+      print(rests.length);
+      return rests;
+ }
 
 
 class _SearchDepoState extends State<SearchDepo> {
+  
+TextEditingController search = new TextEditingController();
+    String searchit = "";
+  @override
+  void initState() {
+    super.initState();
+  
+  }
+
+  
+
   @override
   Widget build(BuildContext context) {
+    
+
+
     return Scaffold(
         body: Container(
           decoration: BoxDecoration(
@@ -19,27 +61,16 @@ class _SearchDepoState extends State<SearchDepo> {
           child: Stack(
             children: <Widget>[
                 backButt(),
-                Container(
-                  padding: const EdgeInsets.only(top: 250.0,),
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(50.0),
-                    ),
-                  ),
-                  // child: listData(),
-                ),
+                listData(),
             ],
           ),
         ),
     );
 
-   
-
-
   }
+
+
+
 
     Widget backButt(){
         return new Container(
@@ -56,6 +87,7 @@ class _SearchDepoState extends State<SearchDepo> {
                   ),
                       alignment: Alignment.centerLeft,
                       child: TextField(
+                        controller: search,
                         style: TextStyle(
                           color: Colors.black,
                         ),
@@ -68,7 +100,12 @@ class _SearchDepoState extends State<SearchDepo> {
                           ),
                           hintText: "Search",
                         ),
-                      ),
+                        onChanged: (input){
+                          setState(() {
+                              searchit = input;
+                          });
+                        },
+                      )
                 ),
               
               ),
@@ -77,11 +114,95 @@ class _SearchDepoState extends State<SearchDepo> {
         );
     }
     Widget listData(){
-        return new ListView.builder(
-          itemBuilder: null
-          
+        return Padding(
+          padding: const EdgeInsets.only(top: 150.0),
+          child: new Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50.0),
+              ),
+            ),
+            child: FutureBuilder(
+              future: _getRest(),
+              builder: (BuildContext context, AsyncSnapshot snapshot){
+                    if(snapshot.data == null){
+                  return Container(
+                    child: Center(
+                      child: Text("Restaurants Loading"),
+                    ),
+                  );
+                }else{
+                    return ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context , int index){
+                          return snapshot.data[index].restaurantName.contains(searchit)
+                          |snapshot.data[index].address.contains(searchit) ? GestureDetector(
+                            onTap: (){},
+                            child: Padding(
+                              padding: const EdgeInsets.only(left:30.0,right: 30.0,top: 20.0,),
+                              child: Container(
+                                padding: const EdgeInsets.all(8.0),
+                                height: 90.0,
+                                width: MediaQuery.of(context).size.width,
+                                decoration: BoxDecoration(
+                                  color: Colors.amber,
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                child: Row(
+                                  children: <Widget>[
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Container(
+                                        height: 80.0,
+                                        width: 80.0,
+                                        child: CircleAvatar(
+                                          backgroundImage: AssetImage("asset/img/${snapshot.data[index].restaurantName}.png"),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(width: 20.0,),
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: <Widget>[
+                                         Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Text(snapshot.data[index].restaurantName,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                      ),),
+                                    ),
+                                    SizedBox(height: 15.0,),
+                                     Align(
+                                      alignment: Alignment.bottomLeft,
+                                      child: Text(snapshot.data[index].address,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        color: Colors.black54,
+                                        fontWeight: FontWeight.bold,
+                                      ),),
+                                    ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                              ),
+                          ) : Container();
+                        },
+                      );
 
-          
-          );
+
+
+                }
+              },
+            ),
+          ),
+        );
     }
 }
