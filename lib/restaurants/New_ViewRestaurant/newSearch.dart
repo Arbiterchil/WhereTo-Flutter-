3395071a09f35fc5ad85edf2bb87e5a2dd6447ba
Zhai <jaycemico@gmail.com>
2020-08-08 16:39:00 +0800

    N

---
+++ lib/restaurants/New_ViewRestaurant/newSearch.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomSearch extends SearchDelegate{
+  @override
+  List<Widget> buildActions(BuildContext context) {
+     
+      throw UnimplementedError();
+    }
+  
+    @override
+    Widget buildLeading(BuildContext context) {
+     
+      throw UnimplementedError();
+    }
+  
+    @override
+    Widget buildResults(BuildContext context) {
+    
+      throw UnimplementedError();
+    }
+  
+    @override
+    Widget buildSuggestions(BuildContext context) {
+   
+    throw UnimplementedError();
+  }
+
+}
